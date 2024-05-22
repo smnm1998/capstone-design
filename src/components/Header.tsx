a2f5da8d87a9ac46c19499e9fb0d838854ab3e46
smnm1998 <dltpals7498@gmail.com>
2024-05-22 10:55:01 +0900

    fix: 세부사항 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,34 +5,38 @@ import CategoryItemWrapper from './CategoryItemWrapper';
 import DarkModeToggle from './DarkModeToggle';
 
 const Category = styled.div`
-    padding: 10px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Header: React.FC = () => {
-    const isDarkMode = useSelector((state: any) => state.darkMode);
+  const isDarkMode = useSelector((state: any) => state.darkMode);
 
-    return (
-        <HeaderContainer>
-            <Category>
-                    <CategoryItemWrapper text="Logo" />
-                    <CategoryItemWrapper text="Books" />
-                    <CategoryItemWrapper text="Drama" />
-                    <CategoryItemWrapper text="Movie" />
-                    <CategoryItemWrapper text="Music" />
-
-            </Category>
-            <DarkModeToggle />
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <Category>
+        <CategoryItemWrapper text="Logo" />
+        <CategoryItemWrapper text="Books" />
+        <CategoryItemWrapper text="Drama" />
+        <CategoryItemWrapper text="Movie" />
+        <CategoryItemWrapper text="Music" />
+      </Category>
+      <DarkModeToggle />
+    </HeaderContainer>
+  );
 };
 
 export default Header;
